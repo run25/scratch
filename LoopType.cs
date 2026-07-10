34377670900dff5844c4e7b80a9a1986aceb21c4
run25 <t25.okano@gmail.com>
2026-07-10 23:09:23 +0900

    Add LoopType enum for loop categories

---
+++ LoopType.cs
@@ -0,0 +1,12 @@
+namespace ScratchFlow.Models
+{
+    /// <summary>
+    /// ループ種類
+    /// </summary>
+    public enum LoopType
+    {
+        Infinite,      // 無限ループ
+        While,         // 条件付き
+        Repeat         // 指定回数
+    }
+}
